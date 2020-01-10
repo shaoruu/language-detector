e789ed056c650b3235aaa90e562b0b26cb1512cf
dvfcfbgv <21ianh1@tas.tw>
2020-01-11 01:14:04 +0800

    feat: save gitignore

--- src/language_detector.py
+++ src/language_detector.py
@@ -138,6 +138,8 @@ class LanguageDetector(QWidget):
         # removed saved images
         folder = Path('saves')
         for filename in os.listdir(folder):
+            if filename is .gitignore:
+                continue
             file_path = os.path.join(folder, filename)
             try:
                 if os.path.isfile(file_path) or os.path.islink(file_path):
